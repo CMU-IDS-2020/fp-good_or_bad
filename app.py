be2e9025059832a6d32728bf70771ee8b1f8663a
tianyiii <lintianyi1995@gmail.com>
2020-12-09 13:25:31 -0800

    update

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def main():
 		st.markdown("<font color='blue'><b>To start using our app:</b></font>", unsafe_allow_html=True)
 		st.write("      1. Use the sidebar on the left to navigate to the next section: **input & preprocessing**.")
 		st.write("      2. Select a specific **dataset** and feel free to **write something emotional**!")
-		st.write("      3. Adjust the **training hyperparameters**, or selection **two different sets of hyperparameters** to see the entire training process!")
+		st.write("      3. In Training section. Adjust the **training hyperparameters**, or selection **two different sets of hyperparameters** to see the entire training process!")
 		st.write("      4. In predicting section, check out how a neural net can understand your sentiment!")
 		st.write("")
 		st.write("")
